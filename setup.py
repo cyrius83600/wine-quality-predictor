babe021216051cdeaf3531be2479e59c5f88fde9
cyrius83600 <concascyril2@hotmail.com>
2024-12-04 16:05:17 +0100

    Correctifs

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ setuptools.setup(
     description='Wine quality predictor - a packaged machine learning algorithm to predict wine quality',
     packages=setuptools.find_packages(),
     install_requires=[
-        "scikit-learn==0.22.1",
-        "pandas==1.0.1"
+        "joblib-1.4.2",
+        "numpy-2.1.3",
+        "pandas-2.2.3",
+        "python-dateutil-2.9.0.post0",
+         "pytz-2024.2",
+         "scikit-learn-1.5.2",
+         "scipy-1.14.1",
+         "six-1.16.0",
+         "threadpoolctl-3.5.0",
+         "tzdata-2024.2"
     ]
 )
